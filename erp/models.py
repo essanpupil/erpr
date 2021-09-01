dad75be4d84a68e6c93c287d3ed2365a4dac16d7
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2021-09-02 00:44:47 +0700

    Initial erp database

--- erp/models.py
+++ erp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Material(models.Model):
+    name = models.CharField(max_length=255, unique=True, primary_key=True)
+    unit = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MaterialStock(models.Model):
+    material = models.OneToOneField(Material, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return "{}: {} {}".format(self.material.name, self.quantity, self.material.unit)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True, primary_key=True)
+    unit = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ProductStock(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return "{}: {} {}".format(self.product.name, self.quantity, self.product.unit)
+
+
+class ProductRequirement(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = [['product', 'material']]
